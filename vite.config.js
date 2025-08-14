74fbb459ec43521ca471c93e586a6e9125ede851
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-08-14 15:30:34 +0300

    update vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/', // Vercel alt dizinde değilse '/' olmalı
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
