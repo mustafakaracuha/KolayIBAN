7266115b0c052ecc95d29f3f82756d1995de8d11
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-08-15 16:46:00 +0300

    build(deps): update package-lock.json, package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(), tailwindcss()],
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // yeni versiyon çıkınca otomatik yükle
+      workbox: {
+        skipWaiting: true, // SW hemen aktif olsun
+        clientsClaim: true, // Tüm sekmelerde geçerli olsun
+        cleanupOutdatedCaches: true // eski cache'leri sil
+      }
+    }),
+    tailwindcss()
+  ],
   build: {
     outDir: 'dist'
   }
