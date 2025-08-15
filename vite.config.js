bb76ad37de917d1d71032d10cad41f3d68a470c8
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-08-15 16:21:01 +0300

    update vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,13 @@ export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
