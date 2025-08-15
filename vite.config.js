284194cf1fe632029535b46d8c1b926f3e3afed9
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2025-08-15 16:38:22 +0300

    update vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // yeni versiyon çıkınca otomatik yükle
+      workbox: {
+        skipWaiting: true, // SW hemen aktif olsun
+        clientsClaim: true, // Tüm sekmelerde geçerli olsun
+        cleanupOutdatedCaches: true // eski cache'leri sil
+      }
+    })
+  ],
   build: {
     outDir: 'dist'
   }
